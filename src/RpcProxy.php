fca2cef372f733d9f532a8ef0db2476330cddbcb
inhere <in.798@qq.com>
2018-12-25 00:26:11 +0800

    Squashed 'libs/rpc-server/' content from commit faf0a9e
    git-subtree-dir: libs/rpc-server
    git-subtree-split: faf0a9e801ed54f655a6500e7a1c77b3bbe1e82e

---
+++ src/RpcProxy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017/9/10
+ * Time: 上午8:57
+ */
+
+namespace Inhere\Server\Rpc;
+
+/**
+ * Class RpcProxy
+ * @package Inhere\Server\Rpc
+ */
+class RpcProxy
+{
+
+}
---
+++ src/RpcServer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017/9/9
+ * Time: 下午11:09
+ */
+
+namespace Inhere\Server\Rpc;
+
+use Inhere\Server\BuiltIn\TcpServer;
+
+/**
+ * Class RpcServer
+ * @package Inhere\Server\Rpc
+ */
+class RpcServer extends TcpServer
+{
+
+}
