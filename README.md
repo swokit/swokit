ecda4649d33f2fe34d99d79842ebfd718bab3126
inhere <in.798@qq.com>
2018-12-25 00:48:05 +0800

    Squashed 'libs/process/' content from commit 934fe87
    git-subtree-dir: libs/process
    git-subtree-split: 934fe874e5b6d9f043c143f5af4b609ac881997b

---
+++ README.md
@@ -0,0 +1,12 @@
+# process for swoole
+
+
+## install
+
+```bash
+composer require swoole-kit/process
+```
+
+## license
+
+MIT
---
+++ src/ProcessInterface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017/10/10
+ * Time: 下午11:16
+ */
+
+namespace SwooleKit\Process;
+
+use Swoole\Process;
+
+/**
+ * Interface ProcessInterface
+ * @package SwooleKit\Process
+ */
+interface ProcessInterface
+{
+    const PIPE_NOT_CREATE = 0;
+    const PIPE_SOCK_STREAM = 1;
+    const PIPE_SOCK_DGRAM = 2;
+
+    /**
+     * @param Process $process
+     */
+    public function started(Process $process);
+
+    /**
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * @return Process
+     */
+    public function getProcess(): Process;
+}
