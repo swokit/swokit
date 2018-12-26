d893d128d83042c72ec5eb65b17c5c93d0865bcb
inhere <in.798@qq.com>
2018-12-26 23:35:18 +0800

    update some

---
+++ libs/rpc-proto/README.md
@@ -0,0 +1,13 @@
+# RPC proto
+
+RPC data pack protocol library.
+
+## install 
+
+```bash
+composer require swokit/rpc-proto
+```
+
+## license
+
+MIT
---
+++ libs/rpc-proto/src/JSONRpc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2018-12-26
+ * Time: 23:31
+ */
+
+namespace Swokit\Rpc\Proto;
+
+/**
+ * Class JSONRpc
+ * @package Swokit\Rpc\Proto
+ */
+class JSONRpc
+{
+
+}
